c547bf215ec8b3188cec17971b9349fa79a97d02
daihao33 <13924983@qq.com>
2021-12-13 17:18:59 +0800

    add new
    add new

---
+++ CH101/Dialog/main.cpp
@@ -0,0 +1,22 @@
+﻿#include "dialog.h"
+#include <QApplication>
+#include <QLabel>
+#include <QPointer>
+
+int main(int argc, char *argv[])
+{
+    QApplication a(argc, argv);
+    //Dialog w;
+    //w.show();
+
+//    QLabel label("Hello Qt!");
+//    label.show();
+//    label.setAttribute(Qt::WA_DeleteOnClose);    // 这个不能使用，因为这个label是分配到栈上的
+
+    QWidget w;
+    QLabel label("Hello Qt!");
+    label.setParent(&w);
+    w.show();
+
+    return a.exec();
+}
